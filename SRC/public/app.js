403890635dedb239e0538c47e495bbb8f9c8a6f1
Joel Chacon <taktic11@gmail.com>
2016-07-12 13:23:06 -0500

    ng-repeat

--- SRC/public/app.js
+++ SRC/public/app.js
@@ -1,1 +1,16 @@
 var app = angular.module('meanApp', []);
+
+
+app.controller("mainMeanCtrl", function($scope) {
+
+	$scope.posts = [
+
+		{title: 'post 1', upvotes: 5},
+		{title: 'post 2', upvotes: 2},
+		{title: 'post 3', upvotes: 9},
+		{title: 'post 4', upvotes: 15},
+		{title: 'post 5', upvotes: 4}
+
+	];
+
+});
