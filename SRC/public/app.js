ad3de59c44214336baf9e1bc41d13e93659a9865
Joel Chacon <taktic11@gmail.com>
2016-07-14 18:06:47 -0500

    functionality

--- SRC/public/app.js
+++ SRC/public/app.js
@@ -17,10 +17,18 @@ app.controller("mainMeanCtrl", function($scope) {
 		if(!$scope.title || $scope.title === '') {
 			return;
 		}
-		$scope.posts.push({title: $scope.title, upvotes: 0});
+		$scope.posts.push({
+			title: $scope.title,
+			link: $scope.link,
+			upvotes: 0
+		});
+		console.log($scope.link);
+
 		$scope.title = '';
+		$scope.link = '';
 
 	};
+	console.log($scope.posts);
 	$scope.incrementUpvotes = function(post) {
 		post.upvotes += 1;
 	}
