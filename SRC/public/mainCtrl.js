65f6b078a9c11fd4b06bed1c7c643f9922df933c
Joel Chacon <taktic11@gmail.com>
2016-07-20 14:00:51 -0500

    modularized

---
+++ SRC/public/mainCtrl.js
@@ -0,0 +1,35 @@
+app.controller("mainMeanCtrl", function($scope, meanFactory) {
+			//array of objects//
+ //  /*#1*/$scope.posts = [
+
+	// 	{title: 'post 1', upvotes: 5},
+	// 	{title: 'post 2', upvotes: 2},
+	// 	{title: 'post 3', upvotes: 9},
+	// 	{title: 'post 4', upvotes: 15},
+	// 	{title: 'post 5', upvotes: 4}
+
+	// ];
+/*#1.5*/$scope.posts = meanFactory.posts	
+	// begin function //
+  /*#2*/$scope.addPost = function() {
+
+		if(!$scope.title || $scope.title === '') {
+			return;
+		}
+		$scope.posts.push({
+			title: $scope.title,
+			link: $scope.link,
+			upvotes: 0
+		})
+		$scope.title = '';
+		$scope.link = '';
+
+		};
+		console.log($scope.posts);
+	// end function //
+  /*#3*/$scope.incrementUpvotes = function(post) {
+		post.upvotes += 1;
+	}
+
+
+});
